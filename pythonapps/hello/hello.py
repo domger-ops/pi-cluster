6f1e2046e410bfb834bc89e0944a20011ae04cb6
domger-ops <senddommail@gmail.com>
2024-02-06 22:44:25 -0800

    trying to fix crashloopbackoff

--- pythonapps/hello/hello.py
+++ pythonapps/hello/hello.py
@@ -3,9 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+app.run(host='0.0.0.0', port=5000)
